ff171d30194160587f6544b37b32681a3ffea450
matejdusic <matej.dusic@prototyp.digital>
2026-03-01 22:58:32 +0100

    Added UI tests for contact, about us, cart, login and signup

--- tests/homepage.spec.ts
+++ tests/homepage.spec.ts
@@ -103,4 +103,61 @@ test.describe("Homepage", () => {
         .getByRole("button", { name: "Previous" }),
     ).toBeVisible();
   });
+
+  //contact form
+  test("contact form is visible", async ({ page }) => {
+    await page.getByRole('link', { name: 'Contact' }).click();
+    await expect(page.getByRole('heading', { name: 'New message' })).toBeVisible();
+    await expect(page.locator('#recipient-email')).toBeVisible();
+    await expect(page.getByRole('textbox', { name: 'Contact Email: Contact Name:' })).toBeVisible();
+    await expect(page.getByRole('textbox', { name: 'Message:' })).toBeVisible();
+    await expect(page.getByLabel('New message').getByText('Close')).toBeVisible();
+    await expect(page.getByRole('button', { name: 'Send message' })).toBeVisible();
+    await expect(page.getByRole('dialog', { name: 'New message' }).getByLabel('Close')).toBeVisible();
+  });
+
+  //about us
+  test("about us modal is visible", async ({ page }) => {
+    await page.getByRole('link', { name: 'About us' }).click();
+    await expect(page.getByRole('heading', { name: 'About us', exact: true })).toBeVisible();
+    await expect(page.locator('.vjs-poster')).toBeVisible();
+    await expect(page.getByRole('button', { name: 'Play Video' })).toBeVisible();
+    await expect(page.locator('#videoModal').getByText('Close', { exact: true }).first()).toBeVisible();
+    await expect(page.locator('#videoModal').getByLabel('Close').first()).toBeVisible();
+  });
+
+  //cart
+  test("cart is visible", async ({ page }) => {
+    await page.getByRole('link', { name: 'Cart' }).click();
+    await expect(page).toHaveURL('https://www.demoblaze.com/cart.html');
+    await expect(page.getByRole('heading', { name: 'Products' })).toBeVisible();
+    await expect(page.getByRole('columnheader', { name: 'Pic' })).toBeVisible();
+    await expect(page.getByRole('columnheader', { name: 'Title' })).toBeVisible();
+    await expect(page.getByRole('columnheader', { name: 'Price' })).toBeVisible();
+    await expect(page.getByRole('columnheader', { name: 'x' })).toBeVisible();
+    await expect(page.getByRole('heading', { name: 'Total' })).toBeVisible();
+    await expect(page.getByRole('button', { name: 'Place Order' })).toBeVisible();
+  });
+
+  //login
+  test("login modal is visible", async ({ page }) => {
+    await page.getByRole('link', { name: 'Log in' }).click();
+    await expect(page.getByRole('heading', { name: 'Log in' })).toBeVisible();
+    await expect(page.locator('#loginusername')).toBeVisible();
+    await expect(page.locator('#loginpassword')).toBeVisible();
+    await expect(page.getByRole('button', { name: 'Log in' })).toBeVisible();
+    await expect(page.getByLabel('Log in').getByText('Close')).toBeVisible();
+    await expect(page.getByRole('dialog', { name: 'Log in' }).getByLabel('Close')).toBeVisible();
+  });
+
+  //signup
+  test("signup modal is visible", async ({ page }) => {
+    await page.getByRole('link', { name: 'Sign up' }).click();
+    await expect(page.getByRole('heading', { name: 'Sign up' })).toBeVisible();
+    await expect(page.getByRole('textbox', { name: 'Username:' })).toBeVisible();
+    await expect(page.getByRole('textbox', { name: 'Password:' })).toBeVisible();
+    await expect(page.getByRole('button', { name: 'Sign up' })).toBeVisible();
+    await expect(page.getByLabel('Sign up').getByText('Close')).toBeVisible();
+    await expect(page.getByRole('dialog', { name: 'Sign up' }).getByLabel('Close')).toBeVisible();
+  });
 });
