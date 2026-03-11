a7aedc678035139d37656f15798b3d66cae15582
matejdusic <matej.dusic@prototyp.digital>
2026-03-11 14:14:49 +0100

    Added page object to fixture, refactored tests

--- tests/homepage.spec.ts
+++ tests/homepage.spec.ts
@@ -1,20 +1,21 @@
-import { test, expect } from "@playwright/test";
+import { test as base, expect } from "@playwright/test";
 import { HomePage } from "../pages/HomePage";
 
-test.describe("Homepage", () => {
-  test.beforeEach(async ({ page }) => {
+const test = base.extend<{ homePage: HomePage }>({
+  homePage: async ({ page }, use) => {
     const homePage = new HomePage(page);
     await homePage.goto();
-  });
+    await use(homePage);
+  },
+});
 
-  test("homepage loads correctly", async ({ page }) => {
-    const homePage = new HomePage(page);
-    await expect(page).toHaveURL(/demoblaze/);
+test.describe("Homepage", () => {
+  test("homepage loads correctly", async ({ homePage }) => {
+    await expect(homePage.page).toHaveURL(/demoblaze/);
   });
 
   //navbar
-  test("navbar elements are visible", async ({ page }) => {
-    const homePage = new HomePage(page);
+  test("navbar elements are visible", async ({ homePage }) => {
     await expect.soft(homePage.navbar).toBeVisible();
     await expect.soft(homePage.navbarLink("PRODUCT STORE")).toBeVisible();
     await expect.soft(homePage.navbarLink("Home (current)")).toBeVisible();
@@ -26,8 +27,7 @@ test.describe("Homepage", () => {
   });
 
   //footer
-  test("footer is visible", async ({ page }) => {
-    const homePage = new HomePage(page);
+  test("footer is visible", async ({ homePage }) => {
     await expect.soft(homePage.footer).toBeVisible();
     await expect.soft(homePage.footerHeading("About Us")).toBeVisible();
     await expect.soft(homePage.footerHeading("Get in Touch")).toBeVisible();
@@ -35,16 +35,14 @@ test.describe("Homepage", () => {
   });
 
   //categories and products
-  test("categories and products section is visible", async ({ page }) => {
-    const homePage = new HomePage(page);
+  test("categories and products section is visible", async ({ homePage }) => {
     await expect.soft(homePage.categoryLink("Phones")).toBeVisible();
     await expect.soft(homePage.categoryLink("Laptops")).toBeVisible();
     await expect(homePage.categoryLink("Monitors")).toBeVisible();
   });
 
   //product grid item visibility
-  test("product grid items are visible", async ({ page }) => {
-    const homePage = new HomePage(page);
+  test("product grid items are visible", async ({ homePage }) => {
     await expect.soft(homePage.firstProductCard()).toBeVisible();
     await expect.soft(homePage.prevButton()).toBeVisible();
     await expect.soft(homePage.nextButton()).toBeVisible();
@@ -55,175 +53,182 @@ test.describe("Homepage", () => {
   });
 
   //phones category products visibility
-  test("phones category products are visible", async ({ page }) => {
-    const homePage = new HomePage(page);
+  test("phones category products are visible", async ({ homePage }) => {
     await homePage.clickCategory("Phones");
     await homePage.openProductDetails("Samsung galaxy s6");
   });
 
   //laptops category products visibility
-  test("laptops category products are visible", async ({ page }) => {
-    const homePage = new HomePage(page);
+  test("laptops category products are visible", async ({ homePage }) => {
     await homePage.clickCategory("Laptops");
     await homePage.openProductDetails("MacBook air");
   });
 
   //monitors category products visibility
-  test("monitors category products are visible", async ({ page }) => {
-    const homePage = new HomePage(page);
+  test("monitors category products are visible", async ({ homePage }) => {
     await homePage.clickCategory("Monitors");
     await homePage.openProductDetails("Apple monitor 24");
   });
 
   //product details
-  test("product details are visible", async ({ page }) => {
-    const homePage = new HomePage(page);
+  test("product details are visible", async ({ homePage }) => {
     await homePage.clickCategory("Phones");
     await homePage.openProductDetails("Samsung galaxy s6");
     await expect
-      .soft(page.getByRole("heading", { name: "Samsung galaxy s6" }))
+      .soft(homePage.page.getByRole("heading", { name: "Samsung galaxy s6" }))
       .toBeVisible();
     await expect
-      .soft(page.getByRole("heading", { name: "$360 *includes tax" }))
+      .soft(homePage.page.getByRole("heading", { name: "$360 *includes tax" }))
       .toBeVisible();
-    await expect(page.getByRole("link", { name: "Add to cart" })).toBeVisible();
+    await expect(
+      homePage.page.getByRole("link", { name: "Add to cart" }),
+    ).toBeVisible();
   });
 
   //carousel
-  test("carousel is visible", async ({ page }) => {
-    const homePage = new HomePage(page);
+  test("carousel is visible", async ({ homePage }) => {
     await expect
-      .soft(page.getByRole("img", { name: "First slide" }))
+      .soft(homePage.page.getByRole("img", { name: "First slide" }))
       .toBeVisible();
     await expect
       .soft(
-        page
+        homePage.page
           .locator("#carouselExampleIndicators")
           .getByRole("button", { name: "Next" }),
       )
       .toBeVisible();
     await expect(
-      page
+      homePage.page
         .locator("#carouselExampleIndicators")
         .getByRole("button", { name: "Previous" }),
     ).toBeVisible();
   });
 
   //contact form
-  test("contact form is visible", async ({ page }) => {
-    const homePage = new HomePage(page);
-    await page.getByRole("link", { name: "Contact" }).click();
+  test("contact form is visible", async ({ homePage }) => {
+    await homePage.page.getByRole("link", { name: "Contact" }).click();
     await expect
-      .soft(page.getByRole("heading", { name: "New message" }))
+      .soft(homePage.page.getByRole("heading", { name: "New message" }))
       .toBeVisible();
-    await expect.soft(page.locator("#recipient-email")).toBeVisible();
+    await expect.soft(homePage.page.locator("#recipient-email")).toBeVisible();
     await expect
-      .soft(page.getByRole("textbox", { name: "Contact Email: Contact Name:" }))
-      .toBeVisible();
-    await expect
-      .soft(page.getByRole("textbox", { name: "Message:" }))
-      .toBeVisible();
-    await expect
-      .soft(page.getByLabel("New message").getByText("Close"))
+      .soft(
+        homePage.page.getByRole("textbox", {
+          name: "Contact Email: Contact Name:",
+        }),
+      )
       .toBeVisible();
     await expect
-      .soft(page.getByRole("button", { name: "Send message" }))
+      .soft(homePage.page.getByRole("textbox", { name: "Message:" }))
+      .toBeVisible();
+    await expect
+      .soft(homePage.page.getByLabel("New message").getByText("Close"))
+      .toBeVisible();
+    await expect
+      .soft(homePage.page.getByRole("button", { name: "Send message" }))
       .toBeVisible();
     await expect(
-      page.getByRole("dialog", { name: "New message" }).getByLabel("Close"),
+      homePage.page
+        .getByRole("dialog", { name: "New message" })
+        .getByLabel("Close"),
     ).toBeVisible();
   });
 
   //about us
-  test("about us modal is visible", async ({ page }) => {
-    const homePage = new HomePage(page);
-    await page.getByRole("link", { name: "About us" }).click();
+  test("about us modal is visible", async ({ homePage }) => {
+    await homePage.page.getByRole("link", { name: "About us" }).click();
     await expect
-      .soft(page.getByRole("heading", { name: "About us", exact: true }))
+      .soft(
+        homePage.page.getByRole("heading", { name: "About us", exact: true }),
+      )
       .toBeVisible();
-    await expect.soft(page.locator(".vjs-poster")).toBeVisible();
+    await expect.soft(homePage.page.locator(".vjs-poster")).toBeVisible();
     await expect
-      .soft(page.getByRole("button", { name: "Play Video" }))
+      .soft(homePage.page.getByRole("button", { name: "Play Video" }))
       .toBeVisible();
     await expect
       .soft(
-        page.locator("#videoModal").getByText("Close", { exact: true }).first(),
+        homePage.page
+          .locator("#videoModal")
+          .getByText("Close", { exact: true })
+          .first(),
       )
       .toBeVisible();
     await expect(
-      page.locator("#videoModal").getByLabel("Close").first(),
+      homePage.page.locator("#videoModal").getByLabel("Close").first(),
     ).toBeVisible();
   });
 
   //cart
-  test("cart is visible", async ({ page }) => {
-    const homePage = new HomePage(page);
-    await page.getByRole("link", { name: "Cart" }).click();
-    await expect.soft(page).toHaveURL("https://www.demoblaze.com/cart.html");
+  test("cart is visible", async ({ homePage }) => {
+    await homePage.page.getByRole("link", { name: "Cart" }).click();
     await expect
-      .soft(page.getByRole("heading", { name: "Products" }))
+      .soft(homePage.page)
+      .toHaveURL("https://www.demoblaze.com/cart.html");
+    await expect
+      .soft(homePage.page.getByRole("heading", { name: "Products" }))
       .toBeVisible();
     await expect
-      .soft(page.getByRole("columnheader", { name: "Pic" }))
+      .soft(homePage.page.getByRole("columnheader", { name: "Pic" }))
       .toBeVisible();
     await expect
-      .soft(page.getByRole("columnheader", { name: "Title" }))
+      .soft(homePage.page.getByRole("columnheader", { name: "Title" }))
       .toBeVisible();
     await expect
-      .soft(page.getByRole("columnheader", { name: "Price" }))
+      .soft(homePage.page.getByRole("columnheader", { name: "Price" }))
       .toBeVisible();
     await expect
-      .soft(page.getByRole("columnheader", { name: "x" }))
+      .soft(homePage.page.getByRole("columnheader", { name: "x" }))
       .toBeVisible();
     await expect
-      .soft(page.getByRole("heading", { name: "Total" }))
+      .soft(homePage.page.getByRole("heading", { name: "Total" }))
       .toBeVisible();
     await expect(
-      page.getByRole("button", { name: "Place Order" }),
+      homePage.page.getByRole("button", { name: "Place Order" }),
     ).toBeVisible();
   });
 
   //login
-  test("login modal is visible", async ({ page }) => {
-    const homePage = new HomePage(page);
-    await page.getByRole("link", { name: "Log in" }).click();
+  test("login modal is visible", async ({ homePage }) => {
+    await homePage.page.getByRole("link", { name: "Log in" }).click();
     await expect
-      .soft(page.getByRole("heading", { name: "Log in" }))
+      .soft(homePage.page.getByRole("heading", { name: "Log in" }))
       .toBeVisible();
-    await expect.soft(page.locator("#loginusername")).toBeVisible();
-    await expect.soft(page.locator("#loginpassword")).toBeVisible();
+    await expect.soft(homePage.page.locator("#loginusername")).toBeVisible();
+    await expect.soft(homePage.page.locator("#loginpassword")).toBeVisible();
     await expect
-      .soft(page.getByRole("button", { name: "Log in" }))
+      .soft(homePage.page.getByRole("button", { name: "Log in" }))
       .toBeVisible();
     await expect
-      .soft(page.getByLabel("Log in").getByText("Close"))
+      .soft(homePage.page.getByLabel("Log in").getByText("Close"))
       .toBeVisible();
     await expect(
-      page.getByRole("dialog", { name: "Log in" }).getByLabel("Close"),
+      homePage.page.getByRole("dialog", { name: "Log in" }).getByLabel("Close"),
     ).toBeVisible();
   });
 
   //signup
-  test("signup modal is visible", async ({ page }) => {
-    const homePage = new HomePage(page);
-    await page.getByRole("link", { name: "Sign up" }).click();
+  test("signup modal is visible", async ({ homePage }) => {
+    await homePage.page.getByRole("link", { name: "Sign up" }).click();
     await expect
-      .soft(page.getByRole("heading", { name: "Sign up" }))
+      .soft(homePage.page.getByRole("heading", { name: "Sign up" }))
       .toBeVisible();
     await expect
-      .soft(page.getByRole("textbox", { name: "Username:" }))
+      .soft(homePage.page.getByRole("textbox", { name: "Username:" }))
       .toBeVisible();
     await expect(
-      page.getByRole("textbox", { name: "Password:" }),
+      homePage.page.getByRole("textbox", { name: "Password:" }),
     ).toBeVisible();
     await expect
-      .soft(page.getByRole("button", { name: "Sign up" }))
+      .soft(homePage.page.getByRole("button", { name: "Sign up" }))
       .toBeVisible();
     await expect
-      .soft(page.getByLabel("Sign up").getByText("Close"))
+      .soft(homePage.page.getByLabel("Sign up").getByText("Close"))
       .toBeVisible();
     await expect(
-      page.getByRole("dialog", { name: "Sign up" }).getByLabel("Close"),
+      homePage.page
+        .getByRole("dialog", { name: "Sign up" })
+        .getByLabel("Close"),
     ).toBeVisible();
   });
 });
