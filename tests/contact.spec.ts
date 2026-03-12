b1d539d795bd56a03a5e28d302cf9a4c796a04d3
matejdusic <matej.dusic@prototyp.digital>
2026-03-12 12:19:14 +0100

    Added more contact form tests

--- tests/contact.spec.ts
+++ tests/contact.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from "../fixtures/pageFixtures";
 
 test.describe("Contact Modal", () => {
-  //contact form
+  //contact form visibility
   test("contact form is visible", async ({ homePage, contactPage, page }) => {
     await homePage.goto();
     await contactPage.open();
@@ -28,5 +28,48 @@ test.describe("Contact Modal", () => {
     await expect(
       page.getByRole("dialog", { name: "New message" }).getByLabel("Close"),
     ).toBeVisible();
+  });
+
+  test("contact form validation", async ({ homePage, contactPage, page }) => {
+    await homePage.goto();
+    await contactPage.open();
+    await page.locator("#recipient-email").fill("test@testimus.com");
+    await page
+      .getByRole("textbox", { name: "Contact Email: Contact Name:" })
+      .fill("Test User");
+    await page
+      .getByRole("textbox", { name: "Message:" })
+      .fill("This is a test message.");
+    await page.getByRole("button", { name: "Send message" }).click();
+    await expect(homePage.page).toHaveURL(/demoblaze/);
+  });
+
+  test("close button functionality", async ({
+    homePage,
+    contactPage,
+    page,
+  }) => {
+    await homePage.goto();
+    await contactPage.open();
+    await page.getByLabel("New message").getByText("Close").click();
+    await expect(
+      page.getByRole("heading", { name: "New message" }),
+    ).not.toBeVisible();
+  });
+
+  test("close button functionality with X", async ({
+    homePage,
+    contactPage,
+    page,
+  }) => {
+    await homePage.goto();
+    await contactPage.open();
+    await page
+      .getByRole("dialog", { name: "New message" })
+      .getByLabel("Close")
+      .click();
+    await expect(
+      page.getByRole("heading", { name: "New message" }),
+    ).not.toBeVisible();
   });
 });
