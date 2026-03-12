2a66a357f33a8ba28b2c0ae968042914cd79d394
matejdusic <matej.dusic@prototyp.digital>
2026-03-12 12:38:17 +0100

    Updated AboutPage, added more about us tests

--- pages/AboutPage.ts
+++ pages/AboutPage.ts
@@ -1,10 +1,15 @@
-import { type Page } from "@playwright/test";
+import { Locator, type Page } from "@playwright/test";
 
 export class AboutPage {
   readonly page: Page;
 
   constructor(page: Page) {
     this.page = page;
+  }
+
+  //playButton locator
+  playButton(): Locator {
+    return this.page.getByRole('button', { name: 'Play Video' });
   }
 
   async open() {
--- tests/about.spec.ts
+++ tests/about.spec.ts
@@ -5,20 +5,67 @@ test.describe("About Us Modal", () => {
   test("about us modal is visible", async ({ homePage, aboutPage, page }) => {
     await homePage.goto();
     await aboutPage.open();
-    await expect
-      .soft(page.getByRole("heading", { name: "About us", exact: true }))
-      .toBeVisible();
-    await expect.soft(page.locator(".vjs-poster")).toBeVisible();
-    await expect
-      .soft(page.getByRole("button", { name: "Play Video" }))
-      .toBeVisible();
-    await expect
-      .soft(
-        page.locator("#videoModal").getByText("Close", { exact: true }).first(),
-      )
-      .toBeVisible();
+    await expect(page.getByText("About us × Video Player is")).toBeVisible();
+    // await expect
+    //   .soft(page.getByRole("heading", { name: "About us", exact: true }))
+    //   .toBeVisible();
+    // await expect.soft(page.locator(".vjs-poster")).toBeVisible();
+    // await expect
+    //   .soft(page.getByRole("button", { name: "Play Video" }))
+    //   .toBeVisible();
+    // await expect
+    //   .soft(
+    //     page.locator("#videoModal").getByText("Close", { exact: true }).first(),
+    //   )
+    //   .toBeVisible();
+    // await expect(
+    //   page.locator("#videoModal").getByLabel("Close").first(),
+    // ).toBeVisible();
+  });
+
+  test("Video player is visible", async ({ homePage, aboutPage, page }) => {
+    await homePage.goto();
+    await aboutPage.open();
+    await expect(page.locator(".vjs-poster")).toBeVisible();
+  });
+
+  test("Play button is visible", async ({ homePage, aboutPage, page }) => {
+    await homePage.goto();
+    await aboutPage.open();
+    await expect(aboutPage.playButton()).toBeVisible();
+  });
+
+  test("close button functionality", async ({ homePage, aboutPage, page }) => {
+    await homePage.goto();
+    await aboutPage.open();
+    await page
+      .locator("#videoModal")
+      .getByText("Close", { exact: true })
+      .click();
     await expect(
-      page.locator("#videoModal").getByLabel("Close").first(),
-    ).toBeVisible();
+      page.getByRole("heading", { name: "About us", exact: true }),
+    ).not.toBeVisible();
+  });
+
+  test("close button functionality with X", async ({
+    homePage,
+    aboutPage,
+    page,
+  }) => {
+    await homePage.goto();
+    await aboutPage.open();
+    await page.locator("#videoModal").getByLabel("Close").click();
+    await expect(
+      page.getByRole("heading", { name: "About us", exact: true }),
+    ).not.toBeVisible();
+  });
+
+  test("video player functionality", async ({ homePage, aboutPage, page }) => {
+    await homePage.goto();
+    await aboutPage.open();
+    await aboutPage.playButton().click();
+    // Wait for the video to start playing by checking if the play button is hidden
+    await expect(aboutPage.playButton()).toBeHidden();
+    await expect(page.locator("#example-video_html5_api")).toBeVisible();
   });
 });
